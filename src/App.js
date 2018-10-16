2cac5dfc03cd69d6412db67c8271f74e99fec4bc
Dmitry <manxxeta@gmail.com>
2018-10-16 14:43:14 -0700

    editing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Titles from './Titles.js';
 import Form from './Form.js';
+import Weather from './Weather';
 
 import './App.css';
 
@@ -11,7 +12,8 @@ class App extends React.Component {
 
     return (
       <div>
-        
+        <Titles />
+
       </div>
     );
   }
