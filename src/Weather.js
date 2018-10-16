493c4cc1c1189f95878c43ffc64a6a09a71a9b72
Dmitry <manxxeta@gmail.com>
2018-10-16 14:41:44 -0700

    editing

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 const Weather = (props) => {
-    
+
+    return (
+        <div>
+             {/* weather conditions here  */}
+        </div>
+    );
 }
+
+export default Weather;
